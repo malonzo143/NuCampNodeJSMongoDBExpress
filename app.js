3abe395dad5ad6404e6d3298dc7b9bd0010aa8aa
Morgan <57332487+malonzo143@users.noreply.github.com>
2021-03-13 15:25:46 -0600

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const rect = require('./rectangle');
 
-function solveRect(l, w) {
-    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+const mongoose = require('mongoose');
 
-    rect(l, w, (err, rectangle) => {
-        if (err) {
-            console.log('ERROR:', err.message);
-        } else {
-            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
-            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
-        }
-    });
-    console.log('This statement is logged after the call to rect()');
-}
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(5, -3);
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
