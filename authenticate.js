ffb205f7b19efff2fab0d8129a05ac5831358b81
Morgan <57332487+malonzo143@users.noreply.github.com>
2021-03-29 04:27:36 -0500

    OAuth & Passport Facebook
    Removed user.js in routes, should be "users.js"

--- authenticate.js
+++ authenticate.js
@@ -3,17 +3,23 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require('passport-facebook-token');
+
 const config = require('./config');
+
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 exports.getToken = user => {
     return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
+
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
+
 exports.jwtPassport = passport.use(
     new JwtStrategy(
         opts,
@@ -43,3 +49,35 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 }
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ facebookId: profile.id }, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                // if there's no error but the user is already registered
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+)
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-node_modules
-sessions
-bin/server.cert
-bin/server.key
+module.exports = {
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+      clientId: '822343078494530',
+      clientSecret: '92494480485d8029607b6c39c756592c92494480485d8029607b6c39c756592c'
+  }
+}
